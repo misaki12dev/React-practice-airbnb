cd013e82334b8acafda229ddd97fc44243e54994
misaki12dev <misaki.12k@gmail.com>
2022-09-02 11:19:57 +0900

    using map

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data";
 
 export default function App() {
   // <Hero />
+
+  console.log(data);
+
+  const cards = data.map((card) => {
+    return (
+      <Card
+        img={card.coverImg}
+        title={card.title}
+        rating={card.stats.rating}
+        reviewCount={card.stats.reviewCount}
+        location={card.location}
+        price={card.price}
+      />
+    );
+  });
+
   return (
     <div>
       <Navbar />
-      <Card
-        img="athlete.png"
-        rating="5.0"
-        reviewCount={6}
-        country="Japan"
-        title="Life in Tokyo"
-        price={136}
-      />
+      {cards}
     </div>
   );
 }
